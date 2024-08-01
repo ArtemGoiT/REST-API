ae15f85887e8646ec3ec559e51ac831ecb669640
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-08-01 23:32:56 +0200

    www

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,6 +1,6 @@
 import { getAllStudents, getStudentById } from '../services/students.js';
 
-export const getAllStudentsController = async (req, res) => {
+export const getStudentsController = async (req, res) => {
   const student = await getAllStudents();
   res.json({
     status: 200,
@@ -9,7 +9,16 @@ export const getAllStudentsController = async (req, res) => {
   });
 };
 
-export const getStudentByIdController = async (req, res) => {
+export const getStudentByIdController = async (req, res, next) => {
   const { studentId } = req.params;
   const student = await getStudentById(studentId);
+  if (!student) {
+    next(new Error('Student not found'));
+    return;
+  }
+  res.json({
+    status: 200,
+    message: `Successfully found student with id ${studentId}!`,
+    data: student,
+  });
 };
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import pino from 'pino-http';
 import cors from 'cors';
 import studentsRouter from './routers/students.js';
 import { env } from './utils/env.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -26,17 +28,8 @@ export const startServer = () => {
     });
   });
   app.use(studentsRouter);
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
